1d110c947020c33d2948ed2f41a403df0df17291
apix <apixon@outlook.com>
2025-01-03 23:13:58 +0100

    [tmdb] prevent too many unauthorized requests if 401

--- src/lib/server/tmdb/replaceByTmdb.js
+++ src/lib/server/tmdb/replaceByTmdb.js
@@ -2,20 +2,25 @@ import { updated } from "$app/state";
 import { getTmdbInfos } from "./getTmdbInfos.js";
 
 export async function replaceByTmdb(movieList) {
-    const updatedMovieList = await Promise.all(movieList.watched.map(async (movie) => {
+    const updatedMovieList = [];
+    for (const movie of movieList.watched) {
         try {
             const { titre, poster, runtime } = await getTmdbInfos(movie.tmdbId);
-            return {
+            updatedMovieList.push({
                 ...movie,
                 title: titre,
                 coverLink: poster,
                 movieRuntime: runtime,
-            };
+            });
         } catch (error) {
             console.error(`Failed to fetch TMDB info for movie ID ${movie.tmdbId}:`, error);
-            return movie; // Return the original movie object if there's an error
+            if (error.includes("status: 401")) {
+                console.error('Received 401 Unauthorized error, stopping the loop.');
+                break;
+            }
+            updatedMovieList.push(movie); // Return the original movie object if there's an error
         }
-    }));
+    }
     return {
         ...movieList,
         watched: updatedMovieList
