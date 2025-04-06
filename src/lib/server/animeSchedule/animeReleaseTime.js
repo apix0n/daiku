6816d560fbd0a23a2f0eb96ec0a12a20cd1fb10e
apix <apixon@outlook.com>
2025-04-07 01:34:49 +0200

    added timeout for anischedule cuz its down

--- src/lib/server/animeSchedule/animeReleaseTime.js
+++ src/lib/server/animeSchedule/animeReleaseTime.js
@@ -2,29 +2,46 @@ const apiUrl = 'https://animeschedule.net/api/v3';
 
 export async function getAnimeReleaseTime(anilistId) {
     const url = `${apiUrl}/anime?anilist-ids=${anilistId}`;
-    const res = await fetch(url);
-    if (res.status !== 200) {
+    try {
+        const controller = new AbortController();
+        const timeoutId = setTimeout(() => controller.abort(), 1500); // 1.5 seconds timeout
+
+        const res = await fetch(url, {
+            signal: controller.signal
+        });
+
+        clearTimeout(timeoutId);
+
+        if (res.status !== 200) {
+            return null;
+        }
+        const data = await res.json();
+
+        if (!data.anime || data.anime.length === 0) {
+            return null;
+        }
+
+        const anime = data.anime[0];
+        if (!anime.subTime) {
+            return null;
+        }
+
+        const subTime = new Date(anime.subTime);
+        return [subTime.getUTCHours(), subTime.getUTCMinutes()];
+    } catch (error) {
+        if (error.name === 'AbortError') {
+            console.log("animeSchedule | request timeout for ID:", anilistId);
+        }
         return null;
     }
-    const data = await res.json();
-
-    if (!data.anime || data.anime.length === 0) {
-        return null;
-    }
-
-    const anime = data.anime[0];
-    if (!anime.subTime) {
-        return null;
-    }
-
-    const subTime = new Date(anime.subTime);
-    return [subTime.getUTCHours(), subTime.getUTCMinutes()];
 }
 
 export async function applyAnimeReleaseTime(anime) {
+    if (anime.media.status !== "RELEASING") {
+        return;
+    }
     const releaseTime = await getAnimeReleaseTime(anime.media.id);
-    if (releaseTime && anime.media.status === "RELEASING") {
-        console.log("animeSchedule | found release time for", anime.media.title.english || anime.media.title.romaji);
+    if (releaseTime) {
         if (anime.media.nextAiringEpisode?.airingAt) {
             const nextDate = new Date(anime.media.nextAiringEpisode.airingAt * 1000);
             nextDate.setUTCHours(releaseTime[0], releaseTime[1]);
@@ -35,5 +52,6 @@ export async function applyAnimeReleaseTime(anime) {
             lastDate.setUTCHours(releaseTime[0], releaseTime[1]);
             anime.media.lastEpisode.timestamp = Math.floor(lastDate.getTime() / 1000);
         }
+        console.log("animeSchedule | found release time for", anime.media.title.english || anime.media.title.romaji);
     }
 }
